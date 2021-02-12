05ad32c16ff50f0dfc729d9710cc6c4fec2538e4
kupriyanovart <kupriyanovart@gmail.com>
2021-02-12 10:20:26 +0500

    Update greater_common_divisor.py

--- recursion/greater_common_divisor.py
+++ recursion/greater_common_divisor.py
@@ -1,1 +1,18 @@
+# Медленный вариант из-за возможной слишком высокой глубины вложенности
+# Нахождение НОД методом вычитания
+def gcd_v1(a:int, b:int):
+    if a == b:
+        return a
+    elif a > b:
+        return gcd_v1(a-b, b)
+    else: # b > a
+        return gcd_v1(b-a, a)
 
+
+# Нахождение НОД делением
+def gcd_v2(a:int, b:int):
+    # крайний случай
+    if b == 0:
+        return a
+    # рекуррентный случай
+    return gcd_v2(b, a % b)
