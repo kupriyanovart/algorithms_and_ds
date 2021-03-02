75bd4270d28905388ede3fd647d9259fd35239bf
kupriyanovart <kupriyanovart@gmail.com>
2021-03-02 15:24:44 +0500

    Update stack.py
    Исправлена реализация класса Stack
    Реализована функция проверки корректности скобочной последовательности

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,20 +1,50 @@
 class Stack():
-    s = []
+    _s = []
     
     def push(self, x):
-        self.s.append(x)
+        self._s.append(x)
         
     def pop(self):
-        if self.s:
-            return self.s.pop()
+        if self._s:
+            return self._s.pop()
         print('Стек пуст!')
     
     def size(self):
-        return len(self.s)
-    
+        return len(self._s)
+        
+    def is_empty(self):
+        return not bool(len(self._s))
+        
     def top(self):
-        if self.s:
-            return self.s[-1]
+        if self._s:
+            return self._s[-1]
         print('Стек пуст!')
+        
+
+def is_bracket_sequence_correct(s: str):
+    '''
+    Функция проверки корректности скобочной последовательности, состоящей из круглых и квадратных скобок ()[]
+    Корректные скобочные последовательности:
+    A = ""
+    B = (A)
+    C = AB
+    B = [A]
+    '''
+    stack = Stack()
+    for br in s:
+        if br not in '()[]':
+            continue
+        if br in '([':
+            stack.push(br)
+        else:
+            if stack.is_empty():
+                return False
+            left = stack.pop()
+            right = ')' if left == '(' else ']'
+            if br != right:
+                return False
+    return stack.is_empty()
+    
+print(is_bracket_sequence_correct('()[][[[([])]]]'))
         
 
