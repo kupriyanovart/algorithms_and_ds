0fa26067db4abe6dfcbbb452a1f57d64abd42f99
kupriyanovart <kupriyanovart@gmail.com>
2021-03-02 15:55:24 +0500

    Update stack.py
    Реализован алгоритм вычисления результата выражения, записанного с помощью обратной польской нотации (с помощью стека)

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -46,5 +46,34 @@ def is_bracket_sequence_correct(s: str):
     return stack.is_empty()
     
 print(is_bracket_sequence_correct('()[][[[([])]]]'))
-        
+
+
+def reverse_polish_notation(s: list):
+    stack = Stack()
+    operations = '+', '-', '*', '/'
+    for c in s:
+        if type(c) == int or type(c) == float:
+            stack.push(c)
+        else:
+            if c in operations and stack.size() > 1:
+                right = stack.pop()
+                left = stack.pop()
+                if c == '+':
+                    res = left + right
+                elif c == '-':
+                    res = left - right
+                elif c == '*':
+                    res = left * right
+                else:
+                    res = left / right
+                stack.push(res)
+            else:
+                print('Недопустимая операция')
+
+    if stack.size() != 1:
+        print('Недопустимые входные данные')
+        return
+    return stack.pop()
+
+print(reverse_polish_notation([1, 2, 4, '+','-']))
 
