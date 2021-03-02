543c93d1f7b637c898a39763a44bf404e2f172c1
kupriyanovart <kupriyanovart@gmail.com>
2021-03-02 15:03:07 +0500

    Update stack.py
    Оптимизация лишнего

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -17,11 +17,4 @@ class Stack():
             return self.s[-1]
         print('Стек пуст!')
         
-st = Stack()
-print(st.size())
-st.push(2)
-st.push(3)
-print(st.size())
-print(st.top())
-print(st.pop())
-print(st.size())
+
